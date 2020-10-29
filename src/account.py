3ce9e53c2df7e8cf8a0db68a3ec2c5fa2747eb7e
Vinyou Tamprateep <vtamprateep@gmail.com>
2020-10-29 15:28:55 -0400

    add unittest for order

--- src/account.py
+++ src/account.py
@@ -11,6 +11,12 @@ class OrderBuilder:
     def __init__(self):
         self.order_queue = set()
 
+    def __portfolioCheck(self, *args):
+        for df in args:
+            columns = df.columns
+            assert len(columns) == 2, 'Error: extra columns'
+            assert set(columns) == {'ticker', 'quantity'}, 'Error: column names'
+
     def portfolioOrder(self, cur_state: pd.DataFrame, fut_state: pd.DataFrame):
         '''
         Dataframe structure:
@@ -18,14 +24,17 @@ class OrderBuilder:
         quantity: int
         '''
         self.order_queue.clear()
+        self.__portfolioCheck(cur_state, fut_state)
+
         # Left join
-        left_cur_join = cur_state.merge(fut_state, how='left', on='ticker', suffixes=('_cur', '_fut'))
+        left_cur_join = cur_state.merge(fut_state, how='outer', on='ticker', suffixes=('_cur', '_fut')).fillna(0)
+        print(left_cur_join)
         for _, row in left_cur_join.iterrows():
             quantity = row['quantity_cur'] - row['quantity_fut']
             if quantity > 0:
                 self.order_queue.add(
                     Order(
-                        ticker = row['ticker_cur'],
+                        ticker = row['ticker'],
                         quantity = quantity,
                         action = 'SELL',
                         order_type = 'MARKET',
@@ -34,29 +43,23 @@ class OrderBuilder:
             elif quantity < 0:
                 self.order_queue.add(
                     Order(
-                        ticker = row['ticker_cur'],
+                        ticker = row['ticker'],
                         quantity = abs(quantity),
                         action = 'BUY',
                         order_type = 'MARKET',
                     )
                 )
-            
+
         return self.order_queue
 
     # TODO: Create indicatory order builder after I make an indicator strategy
     def indicatorOrder(self):
         pass
 
-
+# TODO: Consider using @property decorator for getter/setter
+@dataclass(frozen=True)
 class Order:
-    def __init__(self, ticker: str, quantity: int, action: str, order_type: str):
-        self.__checkArgs(action=action.upper(), order_type=order_type.upper())
-
-        self.ticker = ticker.upper()
-        self.quantity = quantity
-        self.action = action.upper()
-        self.order_type = order_type.upper()
-
-    def __checkArgs(self, **kwargs):
-        assert kwargs['action'] in ('BUY', 'SELL'), 'Invalid order action.'
-        assert kwargs['order_type'] in ('MARKET', 'LIMIT'), 'Invalid order type.'
+    ticker: str
+    quantity: int
+    action: str
+    order_type: str
