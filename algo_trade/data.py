ef59bc116c98d62d9a90fea6b5e07bedfd9d9701
Vinyou Tamprateep <vtamprateep@gmail.com>
2021-02-13 19:47:51 -0600

    delete dup functions in data.py

--- algo_trade/data.py
+++ algo_trade/data.py
@@ -3,8 +3,8 @@ from abc import ABCMeta, abstractmethod
 import os
 import numpy as np
 import pandas as pd
-import tda
 
+from event import MarketEvent
 
 class DataHandler(object):
     '''
@@ -157,7 +157,7 @@ class HistoricCSVDataHandler(DataHandler):
                 if bar is not None:
                     self.latest_ticker_data[s].append(bar)
 
-        # self.events.put(MarketEvent())
+        self.events.put(MarketEvent())
 
 class TDADataHandler(DataHandler):
     '''
@@ -166,9 +166,10 @@ class TDADataHandler(DataHandler):
     :param client:  TDA api client, based off of tda-api Python package
     :param ticker_list: List of ticker strings
     '''
-    def __init__(self, client, ticker_list):
+    def __init__(self, client, events, ticker_list):
         self.client = client
         self.ticker_list = ticker_list
+        self.events = events
 
         self.client.set_enforce_enums(enforce_enums=False)
         self.ticker_data = dict()
@@ -299,55 +300,4 @@ class TDADataHandler(DataHandler):
                 if bar is not None:
                     self.latest_ticker_data[t].append(bar)
 
-        # self.events.put(MarketEvent())
-    
-
-def get_fundamental(client, symbols):
-    response = client.search_instruments(symbols, 'fundamental').json()
-    
-    if len(response) == 1:
-        return response[symbols]['fundamental']
-
-    result_dict = dict()
-    for key in response:
-        result_dict[key] = response[key]['fundamental']
-
-    return result_dict
-
-def get_price_history(client, symbols, period_type = 'year', period = 1, frequency_type = 'daily', frequency = 1):
-    if type(symbols) == str:
-        response = client.get_price_history(
-            symbols,
-            period_type=period_type,
-            period=period,
-            frequency_type=frequency_type,
-            frequeny=frequency,
-        ).json()
-
-        price_history = pd.DataFrame(
-                data=response['candles'],
-                columns=['date', 'open', 'high', 'low', 'close'],
-            )
-        price_history['Date'] = price_history['Date'].dt.date
-        
-        return price_history
-
-    result_dict = dict()
-    for ticker in symbols:
-        response = client.get_price_history(
-            ticker,
-            period_type=period_type,
-            period=period,
-            frequency_type=frequency_type,
-            frequeny=frequency,
-        ).json()
-
-        price_history = pd.DataFrame(
-                data=response['candles'],
-                columns=['date', 'open', 'high', 'low', 'close'],
-            )
-        price_history['Date'] = price_history['Date'].dt.date
-
-        result_dict[ticker] = price_history
-
-    return result_dict
+        self.events.put(MarketEvent())
